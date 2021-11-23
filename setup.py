cff4da65f98afd1b4b39c92f3c0bef1df9382e90
Niall Egan <niall.egan@databricks.com>
2021-11-23 20:04:07 +0100

    .

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'dbt-core~={}'.format(dbt_core_version),
-        'databricks-sql-connector>=0.9.1',
+        'databricks-sql-connector>=0.9.0',
     ],
     zip_safe=False,
     classifiers=[
