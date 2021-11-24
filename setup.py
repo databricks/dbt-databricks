49c6411d730725b3e97b271a5e217a4153a7f32b
Niall Egan <negan1998@gmail.com>
2021-11-24 21:59:38 +0100

    Add version range for DBSQL Connector (#21)
    ### Description
    
    The minimum released version of `datbaricks-sql-connector` is v0.9.0 (0.9.1, the only other released version, just changed some docs). We should add as a minimum req (shouldn't actually affect any customers, but I had a 0.1.0 version installed on my machine)
    
    ###  Testing
    Running `pip install .` in a new conda env
    Running `pip install -r requirements.txt`
    
    Co-authored-by: Niall Egan <niall.egan@databricks.com>

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'dbt-core~={}'.format(dbt_core_version),
-        'databricks-sql-connector',  # TODO: version range?
+        'databricks-sql-connector>=0.9.0',
     ],
     zip_safe=False,
     classifiers=[
