28eb3d391d1fa8dd03656e142d2d1ff244b20646
Takuya UESHIN <ueshin@databricks.com>
2022-02-04 12:12:36 -0800

    Setup linter checks. (#48)
    ### Description
    
    Setups linter checks.
    
    #### Rename `flake8` tox environment to `linter` and includes:
      - `mypy` check and fix the type hint errors.
      - `black` check and apply black formatter.
    
    To run linter:
    
    ```
    tox -e linter
    ```
    
    #### Setup `black` tox environment to apply black formatter.
    
    To apply black formatter:
    
    ```
    tox -e black
    ```

--- dbt/adapters/databricks/relation.py
+++ dbt/adapters/databricks/relation.py
@@ -7,7 +7,6 @@ from dbt.adapters.spark.relation import SparkRelation
 
 @dataclass(frozen=True, eq=False, repr=False)
 class DatabricksRelation(SparkRelation):
-
-    def __post_init__(self):
+    def __post_init__(self) -> None:
         if self.database != self.schema and self.database:
-            raise RuntimeException('Cannot set database in Databricks!')
+            raise RuntimeException("Cannot set database in Databricks!")
