e0c29ea84689f8a21b01d600da802e2ba76453c6
Daniel Palma <daniel.palma@brooklyndata.co>
2022-06-20 10:55:09 +0200

    Use dispatch pattern in integration test

--- tests/integration/upload_file/macros/upload_file.sql
+++ tests/integration/upload_file/macros/upload_file.sql
@@ -1,3 +1,3 @@
 {% macro upload_file(local_file_path, dbfs_file_path, overwrite, contents, headers) %}
-  {% do adapter.upload_file(local_file_path, dbfs_file_path, overwrite, contents, headers) %}
+  {{ return(adapter.dispatch('upload_file', 'dbt')(local_file_path, dbfs_file_path, overwrite, contents, headers)) }}
 {% endmacro %}
