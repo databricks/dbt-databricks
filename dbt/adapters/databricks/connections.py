f64d209e5d4cdccb32879767c5a6fdbba7d8e302
Takuya UESHIN <ueshin@databricks.com>
2022-07-05 11:29:34 -0700

    Revert #106 (#123)
    ### Description
    
    Reverts #106 to follow dbt-labs/dbt-spark#378.

--- dbt/adapters/databricks/connections.py
+++ dbt/adapters/databricks/connections.py
@@ -1,6 +1,5 @@
 from contextlib import contextmanager
 from dataclasses import dataclass
-import os
 import re
 import time
 from typing import Any, Callable, ClassVar, Dict, Iterator, List, Optional, Sequence, Tuple
@@ -14,7 +13,6 @@ from dbt.contracts.connection import Connection, ConnectionState
 from dbt.events import AdapterLogger
 from dbt.events.functions import fire_event
 from dbt.events.types import ConnectionUsed, SQLQuery, SQLQueryStatus
-from dbt.tracking import DBT_INVOCATION_ENV
 from dbt.utils import DECIMALS
 
 from dbt.adapters.spark.connections import SparkConnectionManager, _is_retryable_error
@@ -246,8 +244,7 @@ class DatabricksConnectionManager(SparkConnectionManager):
                 cls.validate_creds(creds, required_fields)
 
                 dbt_databricks_version = __version__.version
-                dbt_invocation_env = os.getenv(DBT_INVOCATION_ENV) or "manual"
-                user_agent_entry = f"dbt-databricks/{dbt_databricks_version}; {dbt_invocation_env}"
+                user_agent_entry = f"dbt-databricks/{dbt_databricks_version}"
 
                 # TODO: what is the error when a user specifies a catalog they don't have access to
                 conn: DatabricksSQLConnection = dbsql.connect(
