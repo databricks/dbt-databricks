31d590caa158abc32f6772ca41c12f8bd0041b0d
Takuya UESHIN <ueshin@databricks.com>
2022-08-05 11:37:52 -0700

    Retry DatabricksCredentials._connection_keys to show only meaningful configs (#145)
    ### Description
    
    Retry: `DatabricksCredentials._connection_keys` to show only meaningful configs (#138).
    
    Assuming `DatabricksCredentials.connection_info(self, *, with_aliases: bool = False)` is called from only:
    
    - `Profile` with `with_aliases=True`
    - `DebugTask` without `with_aliases` (`False` by default)
    
    Thus, if `with_aliases` is `True`, `DatabricksCredentials._connection_keys` should return the same tuple as the current one for the internal use; otherwise it can use `catalog` to show in the debug output.

---
+++ tests/integration/debug/test_debug.py
@@ -0,0 +1,39 @@
+from io import StringIO
+from unittest import mock
+
+from tests.integration.base import DBTIntegrationTest, use_profile
+
+
+class TestDebug(DBTIntegrationTest):
+    @property
+    def schema(self):
+        return "debug"
+
+    @property
+    def models(self):
+        return "models"
+
+    def run_and_test(self, contains_catalog: bool):
+        with mock.patch("sys.stdout", new=StringIO()) as fake_out:
+            self.run_dbt(["debug"])
+        stdout = fake_out.getvalue()
+        self.assertIn("host: ", stdout)
+        self.assertIn("http_path: ", stdout)
+        self.assertIn("schema: ", stdout)
+        (self.assertIn if contains_catalog else self.assertNotIn)("catalog: ", stdout)
+
+    @use_profile("databricks_cluster")
+    def test_debug_databricks_cluster(self):
+        self.run_and_test(contains_catalog=False)
+
+    @use_profile("databricks_sql_endpoint")
+    def test_debug_databricks_sql_endpoint(self):
+        self.run_and_test(contains_catalog=False)
+
+    @use_profile("databricks_uc_cluster")
+    def test_debug_databricks_uc_cluster(self):
+        self.run_and_test(contains_catalog=True)
+
+    @use_profile("databricks_uc_sql_endpoint")
+    def test_debug_databricks_uc_sql_endpoint(self):
+        self.run_and_test(contains_catalog=True)
