8d14d0daf12c8e8e873e1f445be20acd093da063
Jasper Koning <KoningJasper@users.noreply.github.com>
2022-08-16 22:21:39 +0200

    Include the value of environment variable "DBT_DATABRICKS_INVOCATION_ENV" to the user agent entry. (#155)
    ### Description
    
    Include the value of environment variable "DBT_DATABRICKS_INVOCATION_ENV" to the user agent entry.

--- dbt/adapters/databricks/connections.py
+++ dbt/adapters/databricks/connections.py
@@ -1,5 +1,6 @@
 from contextlib import contextmanager
 from dataclasses import dataclass
+import os
 import re
 import time
 from typing import Any, Callable, ClassVar, Dict, Iterator, List, Optional, Sequence, Tuple
@@ -27,6 +28,8 @@ from databricks.sql.exc import Error as DBSQLError
 logger = AdapterLogger("Databricks")
 
 CATALOG_KEY_IN_SESSION_PROPERTIES = "databricks.catalog"
+DBT_DATABRICKS_INVOCATION_ENV = "DBT_DATABRICKS_INVOCATION_ENV"
+DBT_DATABRICKS_INVOCATION_ENV_REGEX = re.compile("^[A-z0-9\\-]+$")
 
 
 @dataclass
@@ -224,6 +227,14 @@ class DatabricksConnectionManager(SparkConnectionManager):
                 )
 
     @classmethod
+    def validate_invocation_env(cls, invocation_env: str) -> None:
+        # Thrift doesn't allow nested () so we need to ensure that the passed user agent is valid
+        if not DBT_DATABRICKS_INVOCATION_ENV_REGEX.search(invocation_env):
+            raise dbt.exceptions.ValidationException(
+                f"Invalid invocation environment: {invocation_env}"
+            )
+
+    @classmethod
     def open(cls, connection: Connection) -> Connection:
         if connection.state == ConnectionState.OPEN:
             logger.debug("Connection is already open, skipping open.")
@@ -231,6 +242,14 @@ class DatabricksConnectionManager(SparkConnectionManager):
 
         creds: DatabricksCredentials = connection.credentials
         exc: Optional[Exception] = None
+
+        dbt_databricks_version = __version__.version
+        user_agent_entry = f"dbt-databricks/{dbt_databricks_version}"
+
+        invocation_env = os.environ.get(DBT_DATABRICKS_INVOCATION_ENV)
+        if invocation_env is not None and len(invocation_env) > 0:
+            cls.validate_invocation_env(invocation_env)
+            user_agent_entry = f"{user_agent_entry}; {invocation_env}"
 
         for i in range(1 + creds.connect_retries):
             try:
@@ -242,9 +261,6 @@ class DatabricksConnectionManager(SparkConnectionManager):
                 required_fields = ["host", "http_path", "token"]
 
                 cls.validate_creds(creds, required_fields)
-
-                dbt_databricks_version = __version__.version
-                user_agent_entry = f"dbt-databricks/{dbt_databricks_version}"
 
                 # TODO: what is the error when a user specifies a catalog they don't have access to
                 conn: DatabricksSQLConnection = dbsql.connect(
