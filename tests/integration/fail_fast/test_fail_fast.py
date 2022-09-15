29cc3665a798f7b579b6cd99e23b7d7fd19eda72
Takuya UESHIN <ueshin@databricks.com>
2022-09-15 15:46:46 -0700

    Fix cancellation.

---
+++ tests/integration/fail_fast/test_fail_fast.py
@@ -0,0 +1,37 @@
+from dbt.exceptions import FailFastException
+
+from tests.integration.base import DBTIntegrationTest, use_profile
+
+
+class TesFailFast(DBTIntegrationTest):
+    @property
+    def schema(self):
+        return "fail_fast"
+
+    @property
+    def models(self):
+        return "models"
+
+    def test_fail_fast(self):
+        self.run_dbt(["run"])
+
+        with self.assertRaisesRegex(
+            FailFastException, "Failing early due to test failure or runtime error"
+        ):
+            self.run_dbt(["test", "--fail-fast"])
+
+    @use_profile("databricks_cluster")
+    def test_fail_fast_databricks_cluster(self):
+        self.test_fail_fast()
+
+    @use_profile("databricks_uc_cluster")
+    def test_fail_fast_databricks_uc_cluster(self):
+        self.test_fail_fast()
+
+    @use_profile("databricks_sql_endpoint")
+    def test_fail_fast_databricks_sql_endpoint(self):
+        self.test_fail_fast()
+
+    @use_profile("databricks_uc_sql_endpoint")
+    def test_fail_fast_databricks_uc_sql_endpoint(self):
+        self.test_fail_fast()
