3409624b8cf0664f084eb4e9f1c732dfbdfaab15
Takuya UESHIN <ueshin@databricks.com>
2022-09-27 13:23:32 -0700

    Fix.

--- tests/functional/adapter/utils/test_utils.py
+++ tests/functional/adapter/utils/test_utils.py
@@ -7,7 +7,7 @@ from dbt.tests.adapter.utils.test_any_value import BaseAnyValue
 from dbt.tests.adapter.utils.test_bool_or import BaseBoolOr
 from dbt.tests.adapter.utils.test_cast_bool_to_text import BaseCastBoolToText
 from dbt.tests.adapter.utils.test_concat import BaseConcat
-from dbt.tests.adapter.utils.test_current_timestamp import BaseCurrentTimestampNaive
+from dbt.tests.adapter.utils.test_current_timestamp import BaseCurrentTimestampAware
 from dbt.tests.adapter.utils.test_dateadd import BaseDateAdd
 from dbt.tests.adapter.utils.test_datediff import BaseDateDiff
 from dbt.tests.adapter.utils.test_date_trunc import BaseDateTrunc
@@ -63,7 +63,7 @@ class TestConcat(BaseConcat):
     pass
 
 
-class TestCurrentTimestamp(BaseCurrentTimestampNaive):
+class TestCurrentTimestamp(BaseCurrentTimestampAware):
     pass
 
 
