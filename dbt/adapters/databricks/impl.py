285c53149d49bdb14c9351d86afc2cd54df52902
Takuya UESHIN <ueshin@databricks.com>
2022-11-03 16:07:20 -0700

    Partially revert to fix the case where schema config contains uppercase letters.

--- dbt/adapters/databricks/impl.py
+++ dbt/adapters/databricks/impl.py
@@ -131,13 +131,13 @@ class DatabricksAdapter(SparkAdapter):
                     f'Invalid value from "show table extended ...", '
                     f"got {len(row)} values, expected 4"
                 )
-            _, name, _, information = row
+            _schema, name, _, information = row
             rel_type = RelationType.View if "Type: VIEW" in information else RelationType.Table
             is_delta = "Provider: delta" in information
             is_hudi = "Provider: hudi" in information
             relation = self.Relation.create(
                 database=schema_relation.database,
-                schema=schema_relation.schema,
+                schema=_schema,
                 identifier=name,
                 type=rel_type,
                 information=information,
