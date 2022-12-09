13c4d6ea2b40711a086db5147044f33aae222206
Takuya UESHIN <ueshin@databricks.com>
2022-12-08 16:44:21 -0800

    Fix tox.ini to support the latest tox. (#234)
    ### Description
    
    Fixes `tox.ini` to support the latest `tox`.

--- tests/unit/utils.py
+++ tests/unit/utils.py
@@ -216,38 +216,39 @@ def assert_fails_validation(dct, cls):
         cls.from_dict(dct)
 
 
-def generate_name_macros(package):
-    from dbt.contracts.graph.parsed import ParsedMacro
-    from dbt.node_types import NodeType
-
-    name_sql = {}
-    for component in ("database", "schema", "alias"):
-        if component == "alias":
-            source = "node.name"
-        else:
-            source = f"target.{component}"
-        name = f"generate_{component}_name"
-        sql = (
-            f"{{% macro {name}(value, node) %}} "
-            f"{{% if value %}} {{{{ value }}}} "
-            f"{{% else %}} {{{{ {source} }}}} "
-            f"{{% endif %}} "
-            f"{{% endmacro %}}"
-        )
-        name_sql[name] = sql
-
-    for name, sql in name_sql.items():
-        pm = ParsedMacro(
-            name=name,
-            resource_type=NodeType.Macro,
-            unique_id=f"macro.{package}.{name}",
-            package_name=package,
-            original_file_path=normalize("macros/macro.sql"),
-            root_path="./dbt_packages/root",
-            path=normalize("macros/macro.sql"),
-            macro_sql=sql,
-        )
-        yield pm
+# FIXME: Cannot find implementation or library stub for module named "dbt.contracts.graph.parsed"
+# def generate_name_macros(package):
+#     from dbt.contracts.graph.parsed import ParsedMacro
+#     from dbt.node_types import NodeType
+#
+#     name_sql = {}
+#     for component in ("database", "schema", "alias"):
+#         if component == "alias":
+#             source = "node.name"
+#         else:
+#             source = f"target.{component}"
+#         name = f"generate_{component}_name"
+#         sql = (
+#             f"{{% macro {name}(value, node) %}} "
+#             f"{{% if value %}} {{{{ value }}}} "
+#             f"{{% else %}} {{{{ {source} }}}} "
+#             f"{{% endif %}} "
+#             f"{{% endmacro %}}"
+#         )
+#         name_sql[name] = sql
+#
+#     for name, sql in name_sql.items():
+#         pm = ParsedMacro(
+#             name=name,
+#             resource_type=NodeType.Macro,
+#             unique_id=f"macro.{package}.{name}",
+#             package_name=package,
+#             original_file_path=normalize("macros/macro.sql"),
+#             root_path="./dbt_packages/root",
+#             path=normalize("macros/macro.sql"),
+#             macro_sql=sql,
+#         )
+#         yield pm
 
 
 class TestAdapterConversions(TestCase):
@@ -273,91 +274,95 @@ class TestAdapterConversions(TestCase):
         return table
 
 
-def MockMacro(package, name="my_macro", **kwargs):
-    from dbt.contracts.graph.parsed import ParsedMacro
-    from dbt.node_types import NodeType
-
-    mock_kwargs = dict(
-        resource_type=NodeType.Macro,
-        package_name=package,
-        unique_id=f"macro.{package}.{name}",
-        original_file_path="/dev/null",
-    )
-
-    mock_kwargs.update(kwargs)
-
-    macro = mock.MagicMock(spec=ParsedMacro, **mock_kwargs)
-    macro.name = name
-    return macro
-
-
-def MockMaterialization(package, name="my_materialization", adapter_type=None, **kwargs):
-    if adapter_type is None:
-        adapter_type = "default"
-    kwargs["adapter_type"] = adapter_type
-    return MockMacro(package, f"materialization_{name}_{adapter_type}", **kwargs)
-
-
-def MockGenerateMacro(package, component="some_component", **kwargs):
-    name = f"generate_{component}_name"
-    return MockMacro(package, name=name, **kwargs)
-
-
-def MockSource(package, source_name, name, **kwargs):
-    from dbt.node_types import NodeType
-    from dbt.contracts.graph.parsed import ParsedSourceDefinition
-
-    src = mock.MagicMock(
-        __class__=ParsedSourceDefinition,
-        resource_type=NodeType.Source,
-        source_name=source_name,
-        package_name=package,
-        unique_id=f"source.{package}.{source_name}.{name}",
-        search_name=f"{source_name}.{name}",
-        **kwargs,
-    )
-    src.name = name
-    return src
+# FIXME: Cannot find implementation or library stub for module named "dbt.contracts.graph.parsed"
+# def MockMacro(package, name="my_macro", **kwargs):
+#     from dbt.contracts.graph.parsed import ParsedMacro
+#     from dbt.node_types import NodeType
+#
+#     mock_kwargs = dict(
+#         resource_type=NodeType.Macro,
+#         package_name=package,
+#         unique_id=f"macro.{package}.{name}",
+#         original_file_path="/dev/null",
+#     )
+#
+#     mock_kwargs.update(kwargs)
+#
+#     macro = mock.MagicMock(spec=ParsedMacro, **mock_kwargs)
+#     macro.name = name
+#     return macro
+#
+#
+# def MockMaterialization(package, name="my_materialization", adapter_type=None, **kwargs):
+#     if adapter_type is None:
+#         adapter_type = "default"
+#     kwargs["adapter_type"] = adapter_type
+#     return MockMacro(package, f"materialization_{name}_{adapter_type}", **kwargs)
+#
+#
+# def MockGenerateMacro(package, component="some_component", **kwargs):
+#     name = f"generate_{component}_name"
+#     return MockMacro(package, name=name, **kwargs)
 
 
-def MockNode(package, name, resource_type=None, **kwargs):
-    from dbt.node_types import NodeType
-    from dbt.contracts.graph.parsed import ParsedModelNode, ParsedSeedNode
-
-    if resource_type is None:
-        resource_type = NodeType.Model
-    if resource_type == NodeType.Model:
-        cls = ParsedModelNode
-    elif resource_type == NodeType.Seed:
-        cls = ParsedSeedNode
-    else:
-        raise ValueError(f"I do not know how to handle {resource_type}")
-    node = mock.MagicMock(
-        __class__=cls,
-        resource_type=resource_type,
-        package_name=package,
-        unique_id=f"{str(resource_type)}.{package}.{name}",
-        search_name=name,
-        **kwargs,
-    )
-    node.name = name
-    return node
+# FIXME: Cannot find implementation or library stub for module named "dbt.contracts.graph.parsed"
+# def MockSource(package, source_name, name, **kwargs):
+#     from dbt.node_types import NodeType
+#     from dbt.contracts.graph.parsed import ParsedSourceDefinition
+#
+#     src = mock.MagicMock(
+#         __class__=ParsedSourceDefinition,
+#         resource_type=NodeType.Source,
+#         source_name=source_name,
+#         package_name=package,
+#         unique_id=f"source.{package}.{source_name}.{name}",
+#         search_name=f"{source_name}.{name}",
+#         **kwargs,
+#     )
+#     src.name = name
+#     return src
 
 
-def MockDocumentation(package, name, **kwargs):
-    from dbt.node_types import NodeType
-    from dbt.contracts.graph.parsed import ParsedDocumentation
+# FIXME: Cannot find implementation or library stub for module named "dbt.contracts.graph.parsed"
+# def MockNode(package, name, resource_type=None, **kwargs):
+#     from dbt.node_types import NodeType
+#     from dbt.contracts.graph.parsed import ParsedModelNode, ParsedSeedNode
+#
+#     if resource_type is None:
+#         resource_type = NodeType.Model
+#     if resource_type == NodeType.Model:
+#         cls = ParsedModelNode
+#     elif resource_type == NodeType.Seed:
+#         cls = ParsedSeedNode
+#     else:
+#         raise ValueError(f"I do not know how to handle {resource_type}")
+#     node = mock.MagicMock(
+#         __class__=cls,
+#         resource_type=resource_type,
+#         package_name=package,
+#         unique_id=f"{str(resource_type)}.{package}.{name}",
+#         search_name=name,
+#         **kwargs,
+#     )
+#     node.name = name
+#     return node
 
-    doc = mock.MagicMock(
-        __class__=ParsedDocumentation,
-        resource_type=NodeType.Documentation,
-        package_name=package,
-        search_name=name,
-        unique_id=f"{package}.{name}",
-        **kwargs,
-    )
-    doc.name = name
-    return doc
+
+# FIXME: Cannot find implementation or library stub for module named "dbt.contracts.graph.parsed"
+# def MockDocumentation(package, name, **kwargs):
+#     from dbt.node_types import NodeType
+#     from dbt.contracts.graph.parsed import ParsedDocumentation
+#
+#     doc = mock.MagicMock(
+#         __class__=ParsedDocumentation,
+#         resource_type=NodeType.Documentation,
+#         package_name=package,
+#         search_name=name,
+#         unique_id=f"{package}.{name}",
+#         **kwargs,
+#     )
+#     doc.name = name
+#     return doc
 
 
 def load_internal_manifest_macros(config, macro_hook=lambda m: None):
