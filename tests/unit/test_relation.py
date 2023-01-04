08236d8ae8b6c55f2a04139bd5c0c88f138e3e7c
Andre Furlan <andre.furlan@databricks.com>
2023-01-04 15:49:48 -0800

    adding tests

--- tests/unit/test_relation.py
+++ tests/unit/test_relation.py
@@ -2,7 +2,7 @@ import unittest
 
 from jinja2.runtime import Undefined
 
-from dbt.adapters.databricks.relation import DatabricksRelation
+from dbt.adapters.databricks.relation import DatabricksRelation, DatabricksQuotePolicy
 
 
 class TestDatabricksRelation(unittest.TestCase):
@@ -65,3 +65,30 @@ class TestDatabricksRelation(unittest.TestCase):
         self.assertIsNone(relation.database)
         self.assertEqual(relation.schema, "some_schema")
         self.assertEqual(relation.identifier, "some_table")
+
+    def test_render(self):
+        data = {
+            "path": {
+                "database": "some_database",
+                "schema": "some_schema",
+                "identifier": "some_table",
+            },
+            "type": None,
+        }
+
+        relation = DatabricksRelation.from_dict(data)
+        self.assertEqual(
+            relation.get_default_quote_policy(), DatabricksQuotePolicy(True, True, True)
+        )
+        self.assertEqual(relation.render(), "`some_database`.`some_schema`.`some_table`")
+
+        data = {
+            "path": {
+                "schema": "some_schema",
+                "identifier": "some_table",
+            },
+            "type": None,
+        }
+
+        relation = DatabricksRelation.from_dict(data)
+        self.assertEqual(relation.render(), "`some_schema`.`some_table`")
