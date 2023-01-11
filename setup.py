0f5a432f1dd0b9ff0bb1d803b6a38befab222e1c
Takuya UESHIN <ueshin@databricks.com>
2023-01-11 15:42:09 -0800

    Upgrade databricks-sql-connector to 2.2.2

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
     include_package_data=True,
     install_requires=[
         "dbt-spark~={}".format(dbt_spark_version),
-        "databricks-sql-connector>=2.0.5",
+        "databricks-sql-connector>=2.2.2",
     ],
     zip_safe=False,
     classifiers=[
