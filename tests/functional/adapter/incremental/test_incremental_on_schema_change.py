4ed7f925c11a961186feaa5f958e74401d03aa8d
Takuya UESHIN <ueshin@databricks.com>
2023-01-27 14:39:08 -0800

    Fix.

--- tests/functional/adapter/incremental/test_incremental_on_schema_change.py
+++ tests/functional/adapter/incremental/test_incremental_on_schema_change.py
@@ -3,72 +3,12 @@ import pytest
 from dbt.tests.util import run_dbt
 
 from dbt.tests.adapter.incremental.test_incremental_on_schema_change import (
+    BaseIncrementalOnSchemaChange,
     BaseIncrementalOnSchemaChangeSetup,
 )
 
 
-class IncrementalOnSchemaChangeIgnoreFail(BaseIncrementalOnSchemaChangeSetup):
-    def test_run_incremental_ignore(self, project):
-        select = "model_a incremental_ignore incremental_ignore_target"
-        compare_source = "incremental_ignore"
-        compare_target = "incremental_ignore_target"
-        self.run_twice_and_assert(select, compare_source, compare_target, project)
-
-    def test_run_incremental_fail_on_schema_change(self, project):
-        select = "model_a incremental_fail"
-        run_dbt(["run", "--models", select, "--full-refresh"])
-        results_two = run_dbt(["run", "--models", select], expect_pass=False)
-        assert "Compilation Error" in results_two[1].message
-
-
-class TestAppendOnSchemaChange(IncrementalOnSchemaChangeIgnoreFail):
-    @pytest.fixture(scope="class")
-    def project_config_update(self):
-        return {
-            "models": {
-                "+incremental_strategy": "append",
-            }
-        }
-
-
-@pytest.mark.skip_profile(
-    "databricks_uc_cluster", "databricks_sql_endpoint", "databricks_uc_sql_endpoint"
-)
-class TestAppendParquetOnSchemaChange(IncrementalOnSchemaChangeIgnoreFail):
-    @pytest.fixture(scope="class")
-    def project_config_update(self):
-        return {
-            "models": {
-                "+file_format": "parquet",
-                "+incremental_strategy": "append",
-            }
-        }
-
-
-@pytest.mark.skip_profile(
-    "databricks_uc_cluster", "databricks_sql_endpoint", "databricks_uc_sql_endpoint"
-)
-class TestInsertOverwriteOnSchemaChange(IncrementalOnSchemaChangeIgnoreFail):
-    @pytest.fixture(scope="class")
-    def project_config_update(self):
-        return {
-            "models": {
-                "+file_format": "parquet",
-                "+partition_by": "id",
-                "+incremental_strategy": "insert_overwrite",
-            }
-        }
-
-
-class TestMergeOnSchemaChange(BaseIncrementalOnSchemaChangeSetup):
-    @pytest.fixture(scope="class")
-    def project_config_update(self):
-        return {
-            "models": {
-                "+unique_key": "id",
-            }
-        }
-
+class BaseIncrementalOnSchemaChangeDatabricksSetup(BaseIncrementalOnSchemaChangeSetup):
     def run_incremental_sync_all_columns(self, project):
         select = "model_a incremental_sync_all_columns incremental_sync_all_columns_target"
         compare_source = "incremental_sync_all_columns"  # noqa: F841
@@ -87,12 +27,61 @@ class TestMergeOnSchemaChange(BaseIncrementalOnSchemaChangeSetup):
         results = run_dbt(["run", "--models", select], expect_pass=False)
         assert "Compilation Error" in results[1].message
 
+
+class BaseIncrementalOnSchemaChangeDatabricks(
+    BaseIncrementalOnSchemaChange, BaseIncrementalOnSchemaChangeDatabricksSetup
+):
     def test_run_incremental_append_new_columns(self, project):
         # only adding new columns in supported
         self.run_incremental_append_new_columns(project)
         # handling columns that have been removed doesn"t work on Delta Lake today
         # self.run_incremental_append_new_columns_remove_one(project)
 
-    def test_run_incremental_sync_all_columns(self, project):
-        self.run_incremental_sync_all_columns(project)
-        self.run_incremental_sync_remove_only(project)
+
+class TestAppendOnSchemaChangeDatabricks(BaseIncrementalOnSchemaChangeDatabricks):
+    @pytest.fixture(scope="class")
+    def project_config_update(self):
+        return {
+            "models": {
+                "+incremental_strategy": "append",
+            }
+        }
+
+
+@pytest.mark.skip_profile(
+    "databricks_uc_cluster", "databricks_sql_endpoint", "databricks_uc_sql_endpoint"
+)
+class TestAppendParquetOnSchemaChangeDatabricks(BaseIncrementalOnSchemaChangeDatabricks):
+    @pytest.fixture(scope="class")
+    def project_config_update(self):
+        return {
+            "models": {
+                "+file_format": "parquet",
+                "+incremental_strategy": "append",
+            }
+        }
+
+
+@pytest.mark.skip_profile(
+    "databricks_uc_cluster", "databricks_sql_endpoint", "databricks_uc_sql_endpoint"
+)
+class TestInsertOverwriteOnSchemaChangeDatabricks(BaseIncrementalOnSchemaChangeDatabricks):
+    @pytest.fixture(scope="class")
+    def project_config_update(self):
+        return {
+            "models": {
+                "+file_format": "parquet",
+                "+partition_by": "id",
+                "+incremental_strategy": "insert_overwrite",
+            }
+        }
+
+
+class TestMergeOnSchemaChangeDatabricks(BaseIncrementalOnSchemaChangeDatabricks):
+    @pytest.fixture(scope="class")
+    def project_config_update(self):
+        return {
+            "models": {
+                "+unique_key": "id",
+            }
+        }
