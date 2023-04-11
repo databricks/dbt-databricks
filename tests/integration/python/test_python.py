a0b7f4d67a86d902a6c33c1c3ba19537a9d3fde4
Jesse Whitehouse <jesse.whitehouse@databricks.com>
2023-04-11 16:00:12 -0500

    Add integration test for verifying this behaviour.
    Run the test manually with the Python compute resource in TERMINATED or
    TERMINATING state. Manually observe that the model starts the compute
    and runs the model rather than raising a ClusterNotStartException
    
    Signed-off-by: Jesse Whitehouse <jesse.whitehouse@databricks.com>

---
+++ tests/integration/python/test_python.py
@@ -0,0 +1,32 @@
+from tests.integration.base import DBTIntegrationTest, use_profile
+import os, pytest
+
+@pytest.mark.skip(reason="Run manually. Test must start with the Python compute resource in TERMINATED or TERMINATING state")
+class TestPython(DBTIntegrationTest):
+    @property
+    def schema(self):
+        return "python"
+
+    @property
+    def models(self):
+        return "models"
+
+    @property
+    def project_config(self):
+        return {
+            "config-version": 2,
+            "vars":
+                {"http_path": os.getenv("DBT_DATABRICKS_CLUSTER_HTTP_PATH")}
+        }
+
+    def python_exc(self):
+        self.run_dbt(["run"])
+        self.run_dbt(["run"])  # make sure it also run in incremental
+
+    @use_profile("databricks_sql_endpoint")
+    def test_python_databricks_sql_endpoint(self):
+        self.python_exc()
+
+    @use_profile("databricks_uc_sql_endpoint")
+    def test_python_databricks_uc_sql_endpoint(self):
+        self.python_exc()
