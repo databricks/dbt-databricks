487bacbb94a314ee34dec0599eed7c15ca555117
Raymond Cypher <raymond.cypher@databricks.com>
2023-06-14 16:01:17 -0600

    Fixed linting issue
    Signed-off-by: Raymond Cypher <raymond.cypher@databricks.com>

--- dbt/adapters/databricks/connections.py
+++ dbt/adapters/databricks/connections.py
@@ -165,9 +165,7 @@ class DatabricksCredentials(Credentials):
                 )
         if not self.token and self.auth_type != "oauth":
             raise dbt.exceptions.DbtProfileError(
-                (
-                    "The config `auth_type: oauth` is required when not using access token"
-                )
+                ("The config `auth_type: oauth` is required when not using access token")
             )
 
         if not self.client_id and self.client_secret:
@@ -191,9 +189,7 @@ class DatabricksCredentials(Credentials):
         return invocation_env
 
     @classmethod
-    def get_all_http_headers(
-        cls, user_http_session_headers: Dict[str, str]
-    ) -> Dict[str, str]:
+    def get_all_http_headers(cls, user_http_session_headers: Dict[str, str]) -> Dict[str, str]:
         http_session_headers_str: Optional[str] = os.environ.get(
             DBT_DATABRICKS_HTTP_SESSION_HEADERS
         )
@@ -228,17 +224,13 @@ class DatabricksCredentials(Credentials):
     def unique_field(self) -> str:
         return cast(str, self.host)
 
-    def connection_info(
-        self, *, with_aliases: bool = False
-    ) -> Iterable[Tuple[str, Any]]:
+    def connection_info(self, *, with_aliases: bool = False) -> Iterable[Tuple[str, Any]]:
         as_dict = self.to_dict(omit_none=False)
         connection_keys = set(self._connection_keys(with_aliases=with_aliases))
         aliases: List[str] = []
         if with_aliases:
             aliases = [k for k, v in self._ALIASES.items() if v in connection_keys]
-        for key in itertools.chain(
-            self._connection_keys(with_aliases=with_aliases), aliases
-        ):
+        for key in itertools.chain(self._connection_keys(with_aliases=with_aliases), aliases):
             if key in as_dict:
                 yield key, as_dict[key]
 
@@ -311,9 +303,7 @@ class DatabricksCredentials(Credentials):
                 credsdict = keyring.get_password("dbt-databricks", host)
 
                 if credsdict:
-                    provider = SessionCredentials.from_dict(
-                        oauth_client, json.loads(credsdict)
-                    )
+                    provider = SessionCredentials.from_dict(oauth_client, json.loads(credsdict))
                     # if refresh token is expired, this will throw
                     try:
                         if provider.token().valid:
@@ -335,9 +325,7 @@ class DatabricksCredentials(Credentials):
             # save for later
             self._credentials_provider = provider.as_dict()
             try:
-                keyring.set_password(
-                    "dbt-databricks", host, json.dumps(self._credentials_provider)
-                )
+                keyring.set_password("dbt-databricks", host, json.dumps(self._credentials_provider))
             # error with keyring. Maybe machine has no password persistency
             except Exception as e:
                 logger.debug(e)
@@ -368,9 +356,7 @@ class DatabricksCredentials(Credentials):
             scopes=SCOPES,
         )
 
-        return SessionCredentials.from_dict(
-            client=oauth_client, raw=self._credentials_provider
-        )
+        return SessionCredentials.from_dict(client=oauth_client, raw=self._credentials_provider)
 
 
 class DatabricksSQLConnectionWrapper:
@@ -457,9 +443,7 @@ class DatabricksSQLCursorWrapper:
     _user_agent: str
     _creds: DatabricksCredentials
 
-    def __init__(
-        self, cursor: DatabricksSQLCursor, creds: DatabricksCredentials, user_agent: str
-    ):
+    def __init__(self, cursor: DatabricksSQLCursor, creds: DatabricksCredentials, user_agent: str):
         self._cursor = cursor
         self._creds = creds
         self._user_agent = user_agent
@@ -515,9 +499,7 @@ class DatabricksSQLCursorWrapper:
 
         stopped_states = ("COMPLETED", "FAILED", "CANCELED")
         host = self._creds.host
-        headers = (
-            self._cursor.connection.thrift_backend._auth_provider._header_factory()
-        )
+        headers = self._cursor.connection.thrift_backend._auth_provider._header_factory()
         headers["User-Agent"] = self._user_agent
 
         url = f"https://{host}/api/2.1/unity-catalog/tables/{view_name}"
@@ -533,15 +515,11 @@ class DatabricksSQLCursorWrapper:
         state = None
         response = requests.get(url, headers=headers)
         if response.status_code != 200:
-            raise dbt.exceptions.DbtRuntimeError(
-                f"Error getting pipeline info: {pipeline_id}"
-            )
+            raise dbt.exceptions.DbtRuntimeError(f"Error getting pipeline info: {pipeline_id}")
 
         updates = response.json().get("latest_updates", [])
         if len(updates) == 0:
-            raise dbt.exceptions.DbtRuntimeError(
-                f"No update created for pipeline: {pipeline_id}"
-            )
+            raise dbt.exceptions.DbtRuntimeError(f"No update created for pipeline: {pipeline_id}")
         latest_update = updates[0]
         state = latest_update.get("state")
         update_id = latest_update.get("update_id")
@@ -558,25 +536,19 @@ class DatabricksSQLCursorWrapper:
 
             response = requests.get(url, headers=headers)
             if response.status_code != 200:
-                raise dbt.exceptions.DbtRuntimeError(
-                    f"Error getting pipeline info: {pipeline_id}"
-                )
+                raise dbt.exceptions.DbtRuntimeError(f"Error getting pipeline info: {pipeline_id}")
 
             updates = response.json().get("latest_updates", [])
 
             latest_update = self.findUpdate(updates, update_id)
             if not latest_update:
-                raise dbt.exceptions.DbtRuntimeError(
-                    f"unable to find update {update_id}"
-                )
+                raise dbt.exceptions.DbtRuntimeError(f"unable to find update {update_id}")
 
             state = latest_update.get("state")
             print(f"refreshing {view_name}, update: {update_id} {state}")
 
         if exceeded_timeout:
-            raise dbt.exceptions.DbtRuntimeError(
-                "timed out waiting for materialized view refresh"
-            )
+            raise dbt.exceptions.DbtRuntimeError("timed out waiting for materialized view refresh")
 
         if state == "FAILED":
             raise dbt.exceptions.DbtRuntimeError(f"error refreshing {view_name}")
@@ -587,7 +559,7 @@ class DatabricksSQLCursorWrapper:
         return
 
     @classmethod
-    def findUpdate(cls, updates, id):
+    def findUpdate(cls, updates: List, id: str) -> Optional[Dict]:
         matches = [x for x in updates if x.get("update_id") == id]
         if matches:
             return matches[0]
@@ -622,9 +594,7 @@ class DatabricksSQLCursorWrapper:
     def schemas(self, catalog_name: str, schema_name: Optional[str] = None) -> None:
         self._cursor.schemas(catalog_name=catalog_name, schema_name=schema_name)
 
-    def tables(
-        self, catalog_name: str, schema_name: str, table_name: Optional[str] = None
-    ) -> None:
+    def tables(self, catalog_name: str, schema_name: str, table_name: Optional[str] = None) -> None:
         self._cursor.tables(
             catalog_name=catalog_name, schema_name=schema_name, table_name=table_name
         )
@@ -719,9 +689,7 @@ class DatabricksConnectionManager(SparkConnectionManager):
         connection = self.get_thread_connection()
         if auto_begin and connection.transaction_open is False:
             self.begin()
-        fire_event(
-            ConnectionUsed(conn_type=self.TYPE, conn_name=cast_to_str(connection.name))
-        )
+        fire_event(ConnectionUsed(conn_type=self.TYPE, conn_name=cast_to_str(connection.name)))
 
         with self.exception_handler(sql):
             cursor: Optional[DatabricksSQLCursorWrapper] = None
@@ -730,14 +698,10 @@ class DatabricksConnectionManager(SparkConnectionManager):
                 if abridge_sql_log:
                     log_sql = "{}...".format(log_sql[:512])
 
-                fire_event(
-                    SQLQuery(conn_name=cast_to_str(connection.name), sql=log_sql)
-                )
+                fire_event(SQLQuery(conn_name=cast_to_str(connection.name), sql=log_sql))
                 pre = time.time()
 
-                cursor = cast(
-                    DatabricksSQLConnectionWrapper, connection.handle
-                ).cursor()
+                cursor = cast(DatabricksSQLConnectionWrapper, connection.handle).cursor()
                 cursor.execute(sql, bindings)
 
                 fire_event(
@@ -777,16 +741,12 @@ class DatabricksConnectionManager(SparkConnectionManager):
     ) -> Table:
         connection = self.get_thread_connection()
 
-        fire_event(
-            ConnectionUsed(conn_type=self.TYPE, conn_name=cast_to_str(connection.name))
-        )
+        fire_event(ConnectionUsed(conn_type=self.TYPE, conn_name=cast_to_str(connection.name)))
 
         with self.exception_handler(log_sql):
             cursor: Optional[DatabricksSQLCursorWrapper] = None
             try:
-                fire_event(
-                    SQLQuery(conn_name=cast_to_str(connection.name), sql=log_sql)
-                )
+                fire_event(SQLQuery(conn_name=cast_to_str(connection.name), sql=log_sql))
                 pre = time.time()
 
                 handle: DatabricksSQLConnectionWrapper = connection.handle
@@ -811,9 +771,7 @@ class DatabricksConnectionManager(SparkConnectionManager):
             lambda cursor: cursor.schemas(catalog_name=database, schema_name=schema),
         )
 
-    def list_tables(
-        self, database: str, schema: str, identifier: Optional[str] = None
-    ) -> Table:
+    def list_tables(self, database: str, schema: str, identifier: Optional[str] = None) -> Table:
         return self._execute_cursor(
             f"GetTables(database={database}, schema={schema}, identifier={identifier})",
             lambda cursor: cursor.tables(
@@ -842,9 +800,7 @@ class DatabricksConnectionManager(SparkConnectionManager):
         connection_parameters = creds.connection_parameters.copy()  # type: ignore[union-attr]
 
         http_headers: List[Tuple[str, str]] = list(
-            creds.get_all_http_headers(
-                connection_parameters.pop("http_headers", {})
-            ).items()
+            creds.get_all_http_headers(connection_parameters.pop("http_headers", {})).items()
         )
 
         def connect() -> DatabricksSQLConnectionWrapper:
