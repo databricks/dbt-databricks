b65579838cac106c4caaad556c80feff423c392c
Ben Cassell <ben.cassell@databricks.com>
2023-06-20 13:47:27 -0700

    lint

--- tests/unit/python/test_python_submissions.py
+++ tests/unit/python/test_python_submissions.py
@@ -7,9 +7,7 @@ from dbt.adapters.databricks.python_submissions import DBContext
 class TestDatabricksPythonSubmissions(unittest.TestCase):
     @patch("requests.get")
     @patch("requests.post")
-    def test_start_cluster_returns_on_receiving_running_state(
-        self, mock_post, mock_get
-    ):
+    def test_start_cluster_returns_on_receiving_running_state(self, mock_post, mock_get):
         # Mock the start command
         mock_post.return_value.status_code = 200
         # Mock the status command
