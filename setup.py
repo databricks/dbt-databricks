27c3f5be1d3d5d0ee528f3dccb273b8d932356f5
Jesse Whitehouse <jesse.whitehouse@databricks.com>
2023-06-21 17:17:17 -0500

    Experimentally pin to dev version of pysql to test the log behaviour
    Signed-off-by: Jesse Whitehouse <jesse.whitehouse@databricks.com>

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
     include_package_data=True,
     install_requires=[
         "dbt-spark>=1.5.0",
-        "databricks-sql-connector~=2.6.3",
+        "databricks-sql-connector~=2.6.3dev1",
         "databricks-sdk>=0.1.7",
         "keyring>=23.13.0",
     ],
