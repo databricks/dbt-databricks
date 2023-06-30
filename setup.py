1f9f269c258c90d7da4c86b9bc54b0402a0aa900
Jesse Whitehouse <jesse.whitehouse@databricks.com>
2023-06-30 09:32:10 -0400

    Pin dependencies to minor versions
    Signed-off-by: Jesse Whitehouse <jesse.whitehouse@databricks.com>

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
     packages=find_namespace_packages(include=["dbt", "dbt.*"]),
     include_package_data=True,
     install_requires=[
-        "dbt-spark>=1.5.0",
+        "dbt-spark~=1.5.0",
         "databricks-sql-connector~=2.7.0",
         "databricks-sdk>=0.1.7",
         "keyring>=23.13.0",
