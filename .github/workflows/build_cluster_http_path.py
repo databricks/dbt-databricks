4f08243c99480b958e2145d9f4ca8ef29179487f
Ben Cassell <ben.cassell@databricks.com>
2023-08-16 15:36:22 -0700

    does this do anything?

--- .github/workflows/build_cluster_http_path.py
+++ .github/workflows/build_cluster_http_path.py
@@ -2,7 +2,7 @@ import os
 
 workspace_id = os.getenv("DBT_DATABRICKS_HOST_NAME")[4:20]
 cluster_id = os.getenv("TEST_PECO_CLUSTER_ID")
-http_path = f"sql/protocolv1/o/{workspace_id}/{cluster_id}"
+http_path = f"/sql/protocolv1/o/{workspace_id}/{cluster_id}"
 
 # https://stackoverflow.com/a/72225291/5093960
 env_file = os.getenv("GITHUB_ENV")
