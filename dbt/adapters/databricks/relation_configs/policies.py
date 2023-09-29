c5e05d8f1198f1a981798dffc5c7a99dd5ecfc2a
Anders Swanson <anders.swanson@dbtlabs.com>
2023-09-29 16:42:24 -0400

    new policies

---
+++ dbt/adapters/databricks/relation_configs/policies.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+
+from dbt.adapters.base.relation import Policy
+from dbt.dataclass_schema import StrEnum
+
+
+MAX_CHARACTERS_IN_IDENTIFIER = 127
+
+
+class DatabricksRelationType(StrEnum):
+    Table = "table"
+    View = "view"
+    CTE = "cte"
+    MaterializedView = "materializedview"
+    External = "external"
+    StreamingTable = "streamingtable"
+
+@dataclass
+class DatabricksIncludePolicy(Policy):
+    database: bool = True
+    schema: bool = True
+    identifier: bool = True
+
+
+@dataclass
+class DatabricksQuotePolicy(Policy):
+    database: bool = True
+    schema: bool = True
+    identifier: bool = True
+
