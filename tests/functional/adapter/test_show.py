a6481310782c3e000527d71161b5822925bcebbe
Ben Cassell <ben.cassell@databricks.com>
2023-10-26 11:11:48 -0700

    additional tests for 1.7.0

---
+++ tests/functional/adapter/test_show.py
@@ -0,0 +1,9 @@
+from dbt.tests.adapter.dbt_show.test_dbt_show import BaseShowLimit, BaseShowSqlHeader
+
+
+class TestShowLimitDatabricks(BaseShowLimit):
+    pass
+
+
+class TestShowSqlHeaderDatabricks(BaseShowSqlHeader):
+    pass
