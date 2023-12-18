d2579bb42b1896e490f11b2639591fc2f0e32d50
Ben Cassell <ben.cassell@databricks.com>
2023-12-18 15:54:15 -0800

    macro issue fix

--- dbt/include/databricks/macros/relations/materialized_view/create.sql
+++ dbt/include/databricks/macros/relations/materialized_view/create.sql
@@ -3,10 +3,10 @@
   {%- set materialized_view = adapter.materialized_view_from_model(config.model) -%}
   
   create materialized view {{ relation }}
-  {% materialized_view.partition_by.to_sql_clause() -%}
-  {% materialized_view.comment.to_sql_clause() -%}
-  {% materialized_view.tblproperties.to_sql_clause() -%}
-  {% materialized_view.refresh.to_sql_clause() -%}
+  {{ materialized_view.partition_by.to_sql_clause() }}
+  {{ materialized_view.comment.to_sql_clause() }}
+  {{ materialized_view.tblproperties.to_sql_clause() }}
+  {{ materialized_view.refresh.to_sql_clause() }}
   as
     {{ sql }}
 {% endmacro %}
