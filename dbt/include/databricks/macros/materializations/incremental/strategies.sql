48b4855669adba83942277e5296f3e03d88c0858
case-k-git <casekblog@gmail.com>
2024-01-23 23:13:31 +0900

    Add SQL Column Mismatch Validation Into Incremental Append Strategy

--- dbt/include/databricks/macros/materializations/incremental/strategies.sql
+++ dbt/include/databricks/macros/materializations/incremental/strategies.sql
@@ -50,7 +50,7 @@
 
 {% macro get_insert_into_sql(source_relation, target_relation) %}
 
-    {%- set dest_columns = adapter.get_columns_in_relation(target_relation) -%}
+    {%- set dest_columns = adapter.get_columns_in_relation(source_relation) -%}
     {%- set dest_cols_csv = dest_columns | map(attribute='quoted') | join(', ') -%}
     insert into table {{ target_relation }}
     select {{dest_cols_csv}} from {{ source_relation }}
