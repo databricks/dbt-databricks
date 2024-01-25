9cfe971235abc3d2c4ab525ba192b52147192e18
Jacobus Herman <jacobus.herman@otrium.com>
2024-01-25 15:54:39 +0100

    Split tests into different classes due to maintained state per class.

--- tests/unit/macros/materializations/incremental/test_strategy_macros.py
+++ tests/unit/macros/materializations/incremental/test_strategy_macros.py
@@ -8,6 +8,15 @@ from tests.unit.macros.base import MacroTestBase
 
 
 class TestStrategytMacros(MacroTestBase):
+    @pytest.fixture(scope="class", autouse=True)
+    def modify_context(self, default_context) -> None:
+        default_context["adapter"].quote.side_effect = lambda x: x
+        default_context["convert_to_list"] = Mock(
+            side_effect=lambda x, default_value=None: x if x else []
+        )
+        default_context["execute"] = False
+        default_context["run_query"] = Mock(return_value=[])
+
     @pytest.fixture(scope="class")
     def macro_folders_to_load(self) -> list:
         return ["macros/materializations/incremental"]
@@ -16,12 +25,14 @@ class TestStrategytMacros(MacroTestBase):
     def template_name(self) -> str:
         return "strategies.sql"
 
-    def run_add_predicates_macro(
-        self, template, context, predicates, partition_columns
-    ) -> str:
-        context["adapter"].quote.side_effect = lambda x: x
-        context["convert_to_list"] = lambda x, default_value=None: x if x else []
-        return self.run_macro(
+    @pytest.mark.parametrize("partition_columns", [["id"], [], None, "id"])
+    def test_add_dest_table_partition_predicates_returns_input(
+        self, template, partition_columns
+    ):
+        predicates = ["test", "123", "456"]
+        expected = f"{predicates} {predicates}"
+
+        actual = self.run_macro(
             template,
             "add_dest_table_partition_predicates",
             predicates,
@@ -29,37 +40,60 @@ class TestStrategytMacros(MacroTestBase):
             "test_relation",
         )
 
-    @pytest.mark.parametrize("partition_columns", [["id"], [], None, "id"])
-    def test_add_dest_table_partition_predicates_returns_input(
-        self, template, context, partition_columns
-    ):
-        context["execute"] = False
-        context["run_query"] = Mock(return_value=[])
-        predicates = ["test", "123", "456"]
-        expected = f"{predicates} {predicates}"
+        assert actual == expected
 
-        actual = self.run_add_predicates_macro(
-            template, context, predicates, partition_columns
-        )
+    @pytest.mark.parametrize(
+        "func_input, expected", [(1, "1"), (None, "NULL"), ("1", "'1'"), (True, "True")]
+    )
+    def test_to_literal(self, template, func_input, expected):
+        actual = self.run_macro(template, "to_literal", func_input)
 
         assert actual == expected
+
+
+class TestStrategytMacroAddPartitionPredicatesExecutionWithoutQuery(MacroTestBase):
+    @pytest.fixture(scope="class")
+    def macro_folders_to_load(self) -> list:
+        return ["macros/materializations/incremental"]
+
+    @pytest.fixture(scope="class")
+    def template_name(self) -> str:
+        return "strategies.sql"
 
     @pytest.mark.parametrize("partition_columns", [[], None])
     def test_add_dest_table_partition_predicates_no_query_with_no_partition_columns(
-        self, template, context, partition_columns
+        self, template_bundle, partition_columns
     ):
-        context["execute"] = True
-        context["run_query"] = Mock(return_value=[])
+        template_bundle.context["adapter"].quote.side_effect = lambda x: x
+        template_bundle.context["convert_to_list"] = Mock(
+            side_effect=lambda x, default_value=None: x if x else []
+        )
+        template_bundle.context["execute"] = True
+        template_bundle.context["run_query"] = Mock(return_value=[])
         predicates = ["test", "123", "456"]
         expected = f"{predicates} {predicates}"
 
-        actual = self.run_add_predicates_macro(
-            template, context, predicates, partition_columns
+        actual = self.run_macro(
+            template_bundle.template,
+            "add_dest_table_partition_predicates",
+            predicates,
+            partition_columns,
+            "test_relation",
         )
 
         assert actual == expected
 
-    def test_add_dest_table_partition_predicates_correct_query(self, template, context):
+
+class TestStrategytMacroAddPartitionPredicatesExecutionWithQuery(MacroTestBase):
+    @pytest.fixture(scope="class")
+    def macro_folders_to_load(self) -> list:
+        return ["macros/materializations/incremental"]
+
+    @pytest.fixture(scope="class")
+    def template_name(self) -> str:
+        return "strategies.sql"
+
+    def test_add_dest_table_partition_predicates_correct_query(self, template_bundle):
         partition_columns = ["date", "code", "id"]
         soure_name = "test_relation"
         partition_query_statements = ",\n\t    ".join(
@@ -93,8 +127,13 @@ class TestStrategytMacros(MacroTestBase):
 
             raise RuntimeError(f"Unexpected query: {arg}")
 
-        context["execute"] = True
-        context["run_query"] = Mock(side_effect=side_effect)
+        run_query_mock = Mock(side_effect=side_effect)
+        template_bundle.context["execute"] = True
+        template_bundle.context["adapter"].quote.side_effect = lambda x: x
+        template_bundle.context["convert_to_list"] = Mock(
+            side_effect=lambda x, default_value=None: x if x else []
+        )
+        template_bundle.context["run_query"] = run_query_mock
         predicates = None
         expected_predicates = []
 
@@ -108,16 +147,13 @@ class TestStrategytMacros(MacroTestBase):
 
         expected = f"{expected_predicates}"
 
-        actual = self.run_add_predicates_macro(
-            template, context, predicates, partition_columns
+        actual = self.run_macro(
+            template_bundle.template,
+            "add_dest_table_partition_predicates",
+            predicates,
+            partition_columns,
+            soure_name,
         ).replace(r"\n", "")
 
-        assert actual == expected
-
-    @pytest.mark.parametrize(
-        "func_input, expected", [(1, "1"), (None, "NULL"), ("1", "'1'"), (True, "True")]
-    )
-    def test_to_literal(self, template, func_input, expected):
-        actual = self.run_macro(template, "to_literal", func_input)
-
+        run_query_mock.assert_called_once()
         assert actual == expected
