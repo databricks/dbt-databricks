3b90e9093ed695e135967a45a02f7c6965d1ab89
Ben Cassell <ben.cassell@databricks.com>
2024-02-27 16:25:25 -0800

    wip

---
+++ tests/functional/adapter/incremental/test_incremental_on_schema_change.py
@@ -0,0 +1,7 @@
+from dbt.tests.adapter.incremental.test_incremental_on_schema_change import (
+    BaseIncrementalOnSchemaChange,
+)
+
+
+class TestIncrementalOnSchemaChange(BaseIncrementalOnSchemaChange):
+    pass
