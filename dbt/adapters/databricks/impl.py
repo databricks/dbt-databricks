e6a2aff524e6f99c4506055e49067334b165d854
Ben Cassell <ben.cassell@databricks.com>
2024-03-08 13:46:56 -0800

    prep for release

--- dbt/adapters/databricks/impl.py
+++ dbt/adapters/databricks/impl.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from cmath import e
 from collections import defaultdict
 from concurrent.futures import Future
 from contextlib import contextmanager
@@ -818,11 +819,16 @@ class MaterializedViewAPI(RelationAPIBase[MaterializedViewConfig]):
         )
 
         kwargs = {"relation": relation}
-        results["information_schema.views"] = get_first_row(
-            adapter.execute_macro("get_view_description", kwargs=kwargs)
-        )
+        results["information_schema.views"] = cls._get_information_schema_views(adapter, kwargs)
         results["show_tblproperties"] = adapter.execute_macro("fetch_tbl_properties", kwargs=kwargs)
         return results
+
+    @staticmethod
+    def _get_information_schema_views(adapter: DatabricksAdapter, kwargs: Dict[str, Any]) -> Row:
+        row = get_first_row(adapter.execute_macro("get_view_description", kwargs=kwargs))
+        if "view_definition" in row:
+            return row
+        return get_first_row(adapter.execute_macro("get_view_description_alt", kwargs=kwargs))
 
 
 class StreamingTableAPI(RelationAPIBase[StreamingTableConfig]):
