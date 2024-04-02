c3354e8e2af12a0c074f6c8e14d55f1a2f11b7b6
Ben Cassell <ben.cassell@databricks.com>
2024-04-02 10:55:03 -0700

    [skip-ci] fix unit test, update Changelog

--- tests/unit/relation_configs/test_incremental_config.py
+++ tests/unit/relation_configs/test_incremental_config.py
@@ -7,15 +7,13 @@ from dbt.adapters.databricks.relation_configs.tags import TagsConfig
 class TestIncrementalConfig:
     def test_from_results(self):
         results = {
-            "information_schema.tags": {
-                Table(
-                    rows=[
-                        ["tag1", "value1"],
-                        ["tag2", "value2"],
-                    ],
-                    column_names=["tag_name", "tag_value"],
-                )
-            }
+            "information_schema.tags": Table(
+                rows=[
+                    ["tag1", "value1"],
+                    ["tag2", "value2"],
+                ],
+                column_names=["tag_name", "tag_value"],
+            )
         }
 
         config = IncrementalTableConfig.from_results(results)
