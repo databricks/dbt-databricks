9def8ca426673697c168545ce540c2604caa7b27
Ben Cassell <ben.cassell@databricks.com>
2024-05-21 10:00:20 -0700

    fix issue with conn not initialized

--- dbt/adapters/databricks/connections.py
+++ dbt/adapters/databricks/connections.py
@@ -1441,7 +1441,7 @@ class DatabricksConnectionManager(SparkConnectionManager):
                     user_agent=user_agent_entry,
                 )
             except Error as exc:
-                logger.error(ConnectionCreateError(conn, exc))
+                logger.error(ConnectionCreateError(exc))
                 raise
 
         def exponential_backoff(attempt: int) -> int:
@@ -1524,7 +1524,7 @@ class DatabricksConnectionManager(SparkConnectionManager):
                     user_agent=user_agent_entry,
                 )
             except Error as exc:
-                logger.error(ConnectionCreateError(conn, exc))
+                logger.error(ConnectionCreateError(exc))
                 raise
 
         def exponential_backoff(attempt: int) -> int:
