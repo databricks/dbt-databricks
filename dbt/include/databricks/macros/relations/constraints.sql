542a3125efe755d95c9e67647e572c4a9bfba98c
Frank Oosterhuis <frank+github@scriptzone.nl>
2024-07-12 14:25:59 +0200

    #676 | remove now useless hack
    Signed-off-by: Frank Oosterhuis <frank+github@scriptzone.nl>

--- dbt/include/databricks/macros/relations/constraints.sql
+++ dbt/include/databricks/macros/relations/constraints.sql
@@ -79,8 +79,7 @@
 
 {% macro get_constraints_sql(relation, constraints, model, column={}) %}
   {% set statements = [] %}
-  -- Hack so that not null constraints will be applied before primary key constraints
-  {% for constraint in constraints|sort(attribute='type') %}
+  {% for constraint in constraints %}
     {% if constraint %}
       {% set constraint_statements = get_constraint_sql(relation, constraint, model, column) %}
       {% for statement in constraint_statements %}
