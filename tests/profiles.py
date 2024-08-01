fc1498d312abdd9c93806b080f16254b598a89e5
Ben Cassell <ben.cassell@databricks.com>
2024-08-01 09:33:57 -0700

    allow configuring port for tests

--- tests/profiles.py
+++ tests/profiles.py
@@ -39,6 +39,8 @@ def _build_databricks_cluster_target(
         profile["schema"] = schema
     if session_properties is not None:
         profile["session_properties"] = session_properties
+    if os.getenv("DBT_DATABRICKS_PORT"):
+        profile["connection_parameters"] = {"_port": os.getenv("DBT_DATABRICKS_PORT")}
     return profile
 
 
