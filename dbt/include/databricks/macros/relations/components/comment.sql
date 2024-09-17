4ef358cbc59e19232f6e60a294ef47c60a208839
SIDDIAM <aminsiddique95@gmail.com>
2024-09-17 21:57:54 +0530

    fix macro

--- dbt/include/databricks/macros/relations/components/comment.sql
+++ dbt/include/databricks/macros/relations/components/comment.sql
@@ -1,3 +1,10 @@
+{%- macro get_create_sql_comment(comment) -%}
+{% if comment is string -%}
+  COMMENT '{{ comment }}'
+{%- endif -%}
+{%- endmacro -%}
+
+
 {%- macro get_create_column_comment(model) -%}
   {%- if model.columns -%}
     {%- set column_definitions = [] -%}
@@ -9,7 +16,6 @@
 
       {%- do column_definitions.append(column_line) -%}
     {% endfor %}
-
     {{ column_definitions | JOIN(',\n ') }}
   {%- endif -%}
 {%- endmacro -%}
