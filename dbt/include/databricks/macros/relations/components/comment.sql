b3bdf87fb9f9f037ed3bef013e2b1348d1d2f70a
SIDDIAM <amin.siddique@mercedes-benz.com>
2024-09-21 01:47:19 +0530

    dynamically get columns data type if undefined

--- dbt/include/databricks/macros/relations/components/comment.sql
+++ dbt/include/databricks/macros/relations/components/comment.sql
@@ -6,16 +6,48 @@
 
 
 {%- macro get_create_column_comment(model) -%}
-  {%- if model.columns -%}
-    {%- set column_definitions = [] -%}
-    {%- for column_name, column in model.columns.items() -%}
-      {%- set data_type = column.data_type or 'STRING' -%}
-      {%- if column.description -%}
-        {%- set column_line = column_name ~ ' ' ~ data_type ~ ' COMMENT ' ~ "'" ~ column.description ~ "'" -%}
-      {%- endif -%}
 
-      {%- do column_definitions.append(column_line) -%}
-    {% endfor %}
-    {{ column_definitions | JOIN(',\n ') }}
-  {%- endif -%}
+    {%- if model.columns -%}
+        {%- set compiled_sql = modules.re.sub('(?i)from\s+stream', 'from', model['compiled_code']) -%}
+
+        {%- set infer_schema_view -%}
+        CREATE OR REPLACE TEMPORARY VIEW temp_{{ model.name }} AS (
+            SELECT * 
+            FROM ({{ compiled_sql }}) 
+            WHERE 1=2
+        )
+        {% endset %}
+
+        {% set run_infer_schema_view = run_query(infer_schema_view) %}
+
+        {%- set data_type_query = "DESCRIBE temp_" ~ model.name -%}
+        {%- set inferred_data_type = run_query(data_type_query) -%}
+
+        {%- set inferred_schema = {} -%}
+
+        {% for row in inferred_data_type.rows %}
+            {%- set _ = inferred_schema.update({ row['col_name']: row['data_type'] }) -%}
+        {% endfor %}
+        
+        {%- set column_definitions = [] -%}
+        
+        {%- for column_name, column in model.columns.items() -%}
+
+            {%- set data_type = column.data_type or inferred_schema.get(column_name) -%}
+
+            {%- if column.description -%}
+                {%- set column_line = column_name ~ ' ' ~ data_type ~ ' COMMENT ' ~ "'" ~ column.description ~ "'" -%}
+            {%- endif -%}
+
+            {%- do column_definitions.append(column_line) -%}
+        
+        {% endfor %}
+        
+        {{ column_definitions | join(',\n    ') }}
+    
+    {%- endif -%}
+    
 {%- endmacro -%}
+
+
+
