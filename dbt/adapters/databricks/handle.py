3994dd25db8a9195e329918d1edb9adfa4eb72b4
Ben Cassell <ben.cassell@databricks.com>
2025-01-24 12:49:22 -0800

    unit testing done

--- dbt/adapters/databricks/handle.py
+++ dbt/adapters/databricks/handle.py
@@ -2,28 +2,27 @@ import decimal
 import re
 import sys
 from collections.abc import Callable, Sequence
+from functools import partial
 from threading import RLock
 from typing import TYPE_CHECKING, Any, Optional
 
 from dbt_common.exceptions import DbtRuntimeError
 
-from databricks.sql import Error
 from databricks.sql.client import Connection, Cursor
 from dbt.adapters.contracts.connection import AdapterResponse
-from dbt.adapters.databricks.events.connection_events import (
-    ConnectionCancel,
-    ConnectionCancelError,
-    ConnectionClose,
-    ConnectionCloseError,
-)
-from dbt.adapters.databricks.events.cursor_events import (
-    CursorClose,
-    CursorCloseError,
-)
+from dbt.adapters.databricks import utils
 from dbt.adapters.databricks.logging import logger
 
 if TYPE_CHECKING:
     pass
+
+
+CursorOp = Callable[[Cursor], None]
+CursorExecOp = Callable[[Cursor], Cursor]
+CursorWrapperOp = Callable[["CursorWrapper"], None]
+ConnectionOp = Callable[[Connection], None]
+LogOp = Callable[[], str]
+FailLogOp = Callable[[Exception], str]
 
 
 class CursorWrapper:
@@ -36,22 +35,30 @@ class CursorWrapper:
         return self._cursor.description
 
     def cancel(self) -> None:
-        if self.open and self._cursor.active_op_handle:
-            self.open = False
-            try:
-                self._cursor.cancel()
-            except Error as exc:
-                logger.warning(ConnectionCancelError(self._conn, exc))
+        if self._cursor.active_op_handle:
+            self._cleanup(
+                Cursor.cancel,
+                lambda: f"{self} - Cancelling",
+                lambda ex: f"{self} - Exception while cancelling: {ex}",
+            )
 
     def close(self) -> None:
+        self._cleanup(
+            Cursor.close,
+            lambda: f"{self} - Closing",
+            lambda ex: f"{self} - Exception while closing: {ex}",
+        )
+
+    def _cleanup(
+        self,
+        cleanup: CursorOp,
+        startLog: LogOp,
+        failLog: FailLogOp,
+    ) -> None:
         if self.open:
             self.open = False
-            logger.debug(CursorClose(self._cursor))
-
-            try:
-                self._cursor.close()
-            except Error as exc:
-                logger.warning(CursorCloseError(self._cursor, exc))
+            logger.debug(startLog())
+            utils.handle_exceptions_as_warning(lambda: cleanup(self._cursor), failLog)
 
     def fetchall(self) -> Sequence[tuple]:
         return self._cursor.fetchall()
@@ -64,6 +71,12 @@ class CursorWrapper:
 
     def get_response(self) -> AdapterResponse:
         return AdapterResponse(_message="OK", query_id=self._cursor.query_id or "N/A")
+
+    def __str__(self) -> str:
+        return (
+            f"Cursor(session-id={self._cursor.connection.get_session_id_hex()}, "
+            f"command-id={self._cursor.query_id})"
+        )
 
 
 class DatabricksHandle:
@@ -83,14 +96,10 @@ class DatabricksHandle:
     def dbr_version(self) -> tuple[int, int]:
         if not self._dbr_version:
             if self._is_cluster:
-                cursor = self._safe_execute(
-                    lambda cursor: cursor.execute(
-                        "SET spark.databricks.clusterUsageTags.sparkVersion"
-                    )
-                )
+                cursor = self._safe_execute(get_dbr_version)
                 results = cursor.fetchone()
-                cursor.close()
                 self._dbr_version = extract_dbr_version(results[1] if results else "")
+                cursor.close()
             else:
                 # Assuming SQL Warehouse uses the latest version.
                 self._dbr_version = (sys.maxsize, sys.maxsize)
@@ -103,60 +112,63 @@ class DatabricksHandle:
         )
 
     def list_schemas(self, database: str, schema: Optional[str] = None) -> CursorWrapper:
-        return self._safe_execute(
-            lambda cursor: cursor.schemas(catalog_name=database, schema_name=schema)
-        )
+        return self._safe_execute(partial(list_schemas, catalog_name=database, schema_name=schema))
 
     def list_tables(self, database: str, schema: str) -> "CursorWrapper":
-        return self._safe_execute(
-            lambda cursor: cursor.tables(catalog_name=database, schema_name=schema)
-        )
+        return self._safe_execute(partial(list_tables, catalog_name=database, schema_name=schema))
 
     def cancel(self) -> None:
-        with self._lock:
-            if self.open:
-                self.open = False
-                logger.debug(ConnectionCancel(self._conn))
-
-                if self._cursor:
-                    self._cursor.cancel()
+        self._cleanup(
+            Connection.close,
+            CursorWrapper.cancel,
+            lambda: f"{self} - Cancelling",
+            lambda ex: f"{self} - Exception while cancelling: {ex}",
+        )
 
     def close(self) -> None:
-        with self._lock:
-            if self.open:
-                self.open = False
-                logger.debug(ConnectionClose(self._conn))
-
-                try:
-                    self._conn.close()
-                except Error as exc:
-                    logger.warning(ConnectionCloseError(self._conn, exc))
+        self._cleanup(
+            Connection.close,
+            CursorWrapper.close,
+            lambda: f"{self} - Closing",
+            lambda ex: f"{self} - Exception while closing: {ex}",
+        )
 
     def rollback(self) -> None:
         logger.debug("NotImplemented: rollback")
 
-    def _safe_execute(self, f: Callable[[Cursor], Cursor]) -> CursorWrapper:
+    def _cleanup(
+        self,
+        connect_op: ConnectionOp,
+        cursor_op: CursorWrapperOp,
+        startLog: LogOp,
+        failLog: FailLogOp,
+    ) -> None:
+        if self.open:
+            self.open = False
+            logger.debug(startLog())
+
+            if self._cursor:
+                cursor_op(self._cursor)
+
+            utils.handle_exceptions_as_warning(lambda: connect_op(self._conn), failLog)
+
+    def _safe_execute(self, f: CursorExecOp) -> CursorWrapper:
         with self._lock:
             if not self.open:
                 raise DbtRuntimeError("Attempting to execute on a closed connection")
-            if self._cursor and self._cursor.open:
+            if self._cursor:
                 self._cursor.close()
             self._cursor = CursorWrapper(f(self._conn.cursor()))
             return self._cursor
 
     def __del__(self) -> None:
-        if self._cursor and self._cursor.open:
-            # This should not happen. The cursor should explicitly be closed.
-            logger.debug(CursorClose(self._cursor._cursor))
-
+        if self._cursor:
             self._cursor.close()
-            logger.warning("A cursor was closed by destructor.")
 
-        if self.open:
-            logger.debug(ConnectionClose(self._conn))
+        self.close()
 
-            self._conn.close()
-            logger.warning("A connection was closed by destructor.")
+    def __str__(self) -> str:
+        return f"Connection(session-id={self._conn.get_session_id_hex() or 'Unknown'})"
 
 
 def clean_sql(sql: str) -> str:
@@ -166,6 +178,18 @@ def clean_sql(sql: str) -> str:
     return cleaned
 
 
+def get_dbr_version(cursor: Cursor) -> Cursor:
+    return cursor.execute("SET spark.databricks.clusterUsageTags.sparkVersion")
+
+
+def list_schemas(cursor: Cursor, catalog_name: str, schema_name: Optional[str]) -> Cursor:
+    return cursor.schemas(catalog_name=catalog_name, schema_name=schema_name)
+
+
+def list_tables(cursor: Cursor, catalog_name: str, schema_name: str) -> Cursor:
+    return cursor.tables(catalog_name=catalog_name, schema_name=schema_name)
+
+
 DBR_VERSION_REGEX = re.compile(r"([1-9][0-9]*)\.(x|0|[1-9][0-9]*)")
 
 
@@ -173,10 +197,10 @@ def extract_dbr_version(version: str) -> tuple[int, int]:
     m = DBR_VERSION_REGEX.search(version)
     if m:
         major = int(m.group(1))
-        try:
-            minor = int(m.group(2))
-        except ValueError:
+        if m.group(2) == "x":
             minor = sys.maxsize
+        else:
+            minor = int(m.group(2))
         return (major, minor)
     else:
         raise DbtRuntimeError("Failed to detect DBR version")
--- tests/unit/test_handle.py
+++ tests/unit/test_handle.py
@@ -1,8 +1,19 @@
+import sys
 from decimal import Decimal
+from unittest.mock import Mock, patch
 
 import pytest
+from databricks.sql.client import Cursor
+from dbt_common.exceptions import DbtRuntimeError
 
-from dbt.adapters.databricks.handle import clean_sql, translate_bindings
+from dbt.adapters.contracts.connection import AdapterResponse
+from dbt.adapters.databricks.handle import (
+    CursorWrapper,
+    DatabricksHandle,
+    clean_sql,
+    extract_dbr_version,
+    translate_bindings,
+)
 
 
 class TestHandle:
@@ -17,3 +28,196 @@ class TestHandle:
     )
     def test_clean_sql(self, sql, expected):
         assert clean_sql(sql) == expected
+
+    @pytest.mark.parametrize("result, expected", [("14.x", (14, sys.maxsize)), ("12.1", (12, 1))])
+    def test_extract_dbr_version(self, result, expected):
+        assert extract_dbr_version(result) == expected
+
+    def test_extract_dbr_version__invalid(self):
+        with pytest.raises(DbtRuntimeError):
+            extract_dbr_version("foo")
+
+
+class TestCursorWrapper:
+    @pytest.fixture
+    def cursor(self):
+        return Mock()
+
+    def test_description(self, cursor):
+        cursor.description = [("foo", "bar")]
+        wrapper = CursorWrapper(cursor)
+        assert wrapper.description == [("foo", "bar")]
+
+    def test_cancel__closed(self, cursor):
+        with patch("dbt.adapters.databricks.handle.Cursor.cancel") as cancel:
+            wrapper = CursorWrapper(cursor)
+            wrapper.open = False
+            wrapper.cancel()
+            cancel.assert_not_called()
+
+    def test_cancel__open_no_result_set(self, cursor):
+        wrapper = CursorWrapper(cursor)
+        cursor.active_result_set = None
+        wrapper.cancel()
+        assert wrapper.open is False
+
+    def test_cancel__open_with_result_set(self, cursor):
+        with patch("dbt.adapters.databricks.handle.Cursor.cancel") as cancel:
+            wrapper = CursorWrapper(cursor)
+            wrapper.cancel()
+            cancel.assert_called_once()
+
+    def test_cancel__error_cancelling(self, cursor):
+        with patch("dbt.adapters.databricks.handle.Cursor.cancel") as cancel:
+            cancel.side_effect = Exception("foo")
+            wrapper = CursorWrapper(cursor)
+            wrapper.cancel()
+            cancel.assert_called_once()
+
+    def test_closed__closed(self, cursor):
+        wrapper = CursorWrapper(cursor)
+        wrapper.open = False
+        wrapper.close()
+        cursor.close.assert_not_called()
+
+    def test_closed__open(self, cursor):
+        wrapper = CursorWrapper(cursor)
+        cursor.active_result_set = None
+        wrapper.close()
+        assert wrapper.open is False
+
+    def test_close__error_closing(self, cursor):
+        with patch("dbt.adapters.databricks.handle.Cursor.close") as close:
+            close.side_effect = Exception("foo")
+            wrapper = CursorWrapper(cursor)
+            wrapper.close()
+            close.assert_called_once()
+
+    def test_fetchone(self, cursor):
+        cursor.fetchone.return_value = [("foo", "bar")]
+        wrapper = CursorWrapper(cursor)
+        assert wrapper.fetchone() == [("foo", "bar")]
+
+    def test_fetchall(self, cursor):
+        cursor.fetchall.return_value = [("foo", "bar")]
+        wrapper = CursorWrapper(cursor)
+        assert wrapper.fetchall() == [("foo", "bar")]
+
+    def test_fetchmany(self, cursor):
+        cursor.fetchmany.return_value = [("foo", "bar")]
+        wrapper = CursorWrapper(cursor)
+        assert wrapper.fetchmany(1) == [("foo", "bar")]
+
+    def test_get_response__no_query_id(self, cursor):
+        cursor.query_id = None
+        wrapper = CursorWrapper(cursor)
+        assert wrapper.get_response() == AdapterResponse("OK", query_id="N/A")
+
+    def test_get_response__with_query_id(self, cursor):
+        cursor.query_id = "id"
+        wrapper = CursorWrapper(cursor)
+        assert wrapper.get_response() == AdapterResponse("OK", query_id="id")
+
+
+class TestDatabricksHandle:
+    @pytest.fixture
+    def conn(self):
+        return Mock()
+
+    @pytest.fixture
+    def cursor(self):
+        return Mock()
+
+    def test_safe_execute__closed(self, conn):
+        handle = DatabricksHandle(conn, True)
+        handle.open = False
+        with pytest.raises(DbtRuntimeError, match="Attempting to execute on a closed connection"):
+            handle._safe_execute(Mock())
+
+    def test_safe_execute__with_cursor(self, conn, cursor):
+        new_cursor = Mock()
+
+        def f(_: Cursor) -> Cursor:
+            return new_cursor
+
+        handle = DatabricksHandle(conn, True)
+        handle._cursor = cursor
+        assert handle._safe_execute(f)._cursor == new_cursor
+        assert handle._cursor._cursor == new_cursor
+        cursor.close.assert_called_once()
+
+    def test_safe_execute__without_cursor(self, conn):
+        new_cursor = Mock()
+
+        def f(_: Cursor) -> Cursor:
+            return new_cursor
+
+        handle = DatabricksHandle(conn, True)
+        assert handle._safe_execute(f)._cursor == new_cursor
+        assert handle._cursor._cursor == new_cursor
+
+    def test_cancel__closed(self, conn):
+        with patch("dbt.adapters.databricks.handle.Connection.close") as close:
+            handle = DatabricksHandle(conn, True)
+            handle.open = False
+            handle.cancel()
+            close.assert_not_called()
+
+    def test_cancel__open_no_cursor(self, conn):
+        with patch("dbt.adapters.databricks.handle.Connection.close") as close:
+            with patch("dbt.adapters.databricks.handle.CursorWrapper.cancel") as cancel:
+                handle = DatabricksHandle(conn, True)
+                handle.cancel()
+                cancel.assert_not_called()
+                close.assert_called_once()
+
+    def test_cancel__open_cursor(self, conn):
+        with patch("dbt.adapters.databricks.handle.Connection.close") as close:
+            with patch("dbt.adapters.databricks.handle.CursorWrapper.cancel") as cancel:
+                handle = DatabricksHandle(conn, True)
+                handle._cursor = Mock()
+                handle.cancel()
+                cancel.assert_called_once()
+                close.assert_called_once()
+
+    def test_cancel__open_raising_exception(self, conn):
+        with patch("dbt.adapters.databricks.handle.Connection.close") as close:
+            with patch("dbt.adapters.databricks.handle.CursorWrapper.cancel") as cancel:
+                close.side_effect = Exception("foo")
+                handle = DatabricksHandle(conn, True)
+                handle.cancel()
+                cancel.assert_not_called()
+                close.assert_called_once()
+
+    def test_close__closed(self, conn):
+        with patch("dbt.adapters.databricks.handle.Connection.close") as close:
+            handle = DatabricksHandle(conn, True)
+            handle.open = False
+            handle.close()
+            close.assert_not_called()
+
+    def test_close__open_no_cursor(self, conn):
+        with patch("dbt.adapters.databricks.handle.Connection.close") as close:
+            with patch("dbt.adapters.databricks.handle.CursorWrapper.close") as cursor_close:
+                handle = DatabricksHandle(conn, True)
+                handle.close()
+                cursor_close.assert_not_called()
+                close.assert_called_once()
+
+    def test_close__open_cursor(self, conn):
+        with patch("dbt.adapters.databricks.handle.Connection.close") as close:
+            with patch("dbt.adapters.databricks.handle.CursorWrapper.close") as cursor_close:
+                handle = DatabricksHandle(conn, True)
+                handle._cursor = Mock()
+                handle.close()
+                cursor_close.assert_called_once()
+                close.assert_called_once()
+
+    def test_close__open_raising_exception(self, conn):
+        with patch("dbt.adapters.databricks.handle.Connection.close") as close:
+            with patch("dbt.adapters.databricks.handle.CursorWrapper.close") as cursor_close:
+                close.side_effect = Exception("foo")
+                handle = DatabricksHandle(conn, True)
+                handle.close()
+                cursor_close.assert_not_called()
+                close.assert_called_once()
