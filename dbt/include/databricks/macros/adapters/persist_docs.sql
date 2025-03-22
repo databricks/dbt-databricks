5528e54361e57043e397275e1e339630ace2b036
Ben Cassell <ben.cassell@databricks.com>
2025-03-21 17:09:13 -0700

    fix run_query_as call site

--- dbt/include/databricks/macros/adapters/persist_docs.sql
+++ dbt/include/databricks/macros/adapters/persist_docs.sql
@@ -5,7 +5,7 @@
       {% set comment = column['description'] %}
       {% set escaped_comment = comment | replace('\'', '\\\'') %}
       {% set column_path = relation.render() ~ '.' ~ api.Column.get_name(column) %}
-      {{ run_query_as(comment_on_column_sql(column_path, escaped_comment)) }}
+      {{ run_query_as(comment_on_column_sql(column_path, escaped_comment), 'alter_column_comment', fetch_result=False) }}
     {% endfor %}
   {% else %}
     {{ log('WARNING - requested to update column comments, but file format ' ~ file_format ~ ' does not support that.') }}
@@ -18,7 +18,7 @@ COMMENT ON COLUMN {{ column_path }} IS '{{ escaped_comment }}'
 
 {% macro databricks__persist_docs(relation, model, for_relation, for_columns) -%}
   {%- if for_relation and config.persist_relation_docs() and model.description %}
-    {{ run_query_as(alter_relation_comment_sql(relation, model)) }}
+    {{ run_query_as(alter_relation_comment_sql(relation, model), 'alter_relation_comment', fetch_result=False) }}
   {% endif %}
   {% if for_columns and config.persist_column_docs() and model.columns %}
     {%- set existing_columns = adapter.get_columns_in_relation(relation) -%}
