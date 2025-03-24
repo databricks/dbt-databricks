e109f76212af0a5e9c79de65e72dc81be4d25e99
Frank Oosterhuis <frank+github@scriptzone.nl>
2025-03-24 17:10:07 +0100

    allow_population_by_field_name -> populate_by_name
    Signed-off-by: Frank Oosterhuis <frank+github@scriptzone.nl>

--- dbt/adapters/databricks/python_models/python_config.py
+++ dbt/adapters/databricks/python_models/python_config.py
@@ -56,4 +56,4 @@ class ParsedPythonModel(BaseModel):
         return f"{self.catalog}-{self.schema_}-{self.identifier}-{uuid.uuid4()}"
 
     class Config:
-        allow_population_by_field_name = True
+        populate_by_name = True
