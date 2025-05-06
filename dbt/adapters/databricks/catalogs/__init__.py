bf20851fe825b2d45ef117fc3c930eddfae44545
Mike Alfare <mike.alfare@dbtlabs.com>
2025-05-06 14:31:24 -0400

    add hive metastore catalog integration

--- dbt/adapters/databricks/catalogs/__init__.py
+++ dbt/adapters/databricks/catalogs/__init__.py
@@ -1,1 +1,2 @@
+from dbt.adapters.databricks.catalogs._hive_metastore import HiveMetastoreCatalogIntegration
 from dbt.adapters.databricks.catalogs._relation import DatabricksCatalogRelation
--- dbt/adapters/databricks/constants.py
+++ dbt/adapters/databricks/constants.py
@@ -7,9 +7,23 @@ DEFAULT_TABLE_FORMAT = "default"
 DEFAULT_FILE_FORMAT = "parquet"
 
 
+HIVE_METASTORE_CATALOG_TYPE = "hive_metastore"
+
+
 DEFAULT_BUILT_IN_CATALOG = SimpleNamespace(
     name="hive_metastore",
     catalog_type="managed",
+    external_volume=None,
+    adapter_properties={
+        "table_format": DEFAULT_TABLE_FORMAT,
+        "file_format": DEFAULT_FILE_FORMAT,
+    },
+)
+
+
+DEFAULT_HIVE_METASTORE_CATALOG = SimpleNamespace(
+    name="hive_metastore",
+    catalog_type=HIVE_METASTORE_CATALOG_TYPE,
     external_volume=None,
     adapter_properties={
         "table_format": DEFAULT_TABLE_FORMAT,
