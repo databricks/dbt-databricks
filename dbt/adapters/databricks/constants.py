a98009028be763ea4b6d47e482f3eefca2d330cb
Mike Alfare <mike.alfare@dbtlabs.com>
2025-05-06 15:00:19 -0400

    remove unused defaults

--- dbt/adapters/databricks/constants.py
+++ dbt/adapters/databricks/constants.py
@@ -3,17 +3,14 @@ from types import SimpleNamespace
 
 BUILT_IN_TABLE_FORMAT = "default"
 ICEBERG_TABLE_FORMAT = "iceberg"
-DEFAULT_TABLE_FORMAT = BUILT_IN_TABLE_FORMAT
 
 
 DELTA_FILE_FORMAT = "delta"
 HUDI_FILE_FORMAT = "hudi"
-DEFAULT_FILE_FORMAT = DELTA_FILE_FORMAT
 
 
 DELTA_CATALOG_TYPE = "delta"
 HIVE_METASTORE_CATALOG_TYPE = "hive_metastore"
-DEFAULT_CATALOG_TYPE = DELTA_CATALOG_TYPE
 
 
 DEFAULT_DELTA_CATALOG = SimpleNamespace(
@@ -21,8 +18,8 @@ DEFAULT_DELTA_CATALOG = SimpleNamespace(
     catalog_type=DELTA_CATALOG_TYPE,
     external_volume=None,
     adapter_properties={
-        "table_format": DEFAULT_TABLE_FORMAT,
-        "file_format": DEFAULT_FILE_FORMAT,
+        "table_format": BUILT_IN_TABLE_FORMAT,
+        "file_format": DELTA_FILE_FORMAT,
     },
 )
 DEFAULT_HIVE_METASTORE_CATALOG = SimpleNamespace(
@@ -30,8 +27,8 @@ DEFAULT_HIVE_METASTORE_CATALOG = SimpleNamespace(
     catalog_type=HIVE_METASTORE_CATALOG_TYPE,
     external_volume=None,
     adapter_properties={
-        "table_format": DEFAULT_TABLE_FORMAT,
-        "file_format": DEFAULT_FILE_FORMAT,
+        "table_format": BUILT_IN_TABLE_FORMAT,
+        "file_format": DELTA_FILE_FORMAT,
     },
 )
 DEFAULT_BUILT_IN_CATALOG = DEFAULT_DELTA_CATALOG
