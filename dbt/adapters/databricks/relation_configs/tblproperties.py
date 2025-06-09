27c2ac722490771761cae28ea14f19aa62feec2a
Eric Jang <eric.jang@databricks.com>
2025-06-09 11:19:35 -0700

    Fix failing integration tests from new tblproperties field

--- dbt/adapters/databricks/relation_configs/tblproperties.py
+++ dbt/adapters/databricks/relation_configs/tblproperties.py
@@ -41,6 +41,7 @@ class TblPropertiesConfig(DatabricksComponentConfig):
         "delta.rowTracking.materializedRowIdColumnName",
         "spark.internal.pipelines.top_level_entry.user_specified_name",
         "delta.columnMapping.maxColumnId",
+        "spark.sql.internal.pipelines.parentTableId",
     ]
 
     def __eq__(self, __value: Any) -> bool:
