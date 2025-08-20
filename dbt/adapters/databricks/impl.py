5587e387604c20ef768bc42bd9b197e93db102e3
Eric Jang <140465251+ericj-db@users.noreply.github.com>
2025-08-20 13:16:15 -0700

    Update dbt/adapters/databricks/impl.py
    Co-authored-by: Thang Long Vu <107926660+longvu-db@users.noreply.github.com>

--- dbt/adapters/databricks/impl.py
+++ dbt/adapters/databricks/impl.py
@@ -132,7 +132,7 @@ USE_REPLACE_ON_FOR_INSERT_OVERWRITE = BehaviorFlag(
     default=False,
     description=(
         "Use the new INSERT REPLACE ON syntax for insert_overwrite incremental strategy."
-        "  When disabled, falls back to traditional INSERT OVERWRITE syntax."
+        "  When disabled, falls back to legacy INSERT OVERWRITE syntax."
     ),
 )  # type: ignore[typeddict-item]
 
