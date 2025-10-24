f431ee1a3ea0152d1c7f6614e2565c95e701710e
Ben Cassell <ben.cassell@databricks.com>
2025-10-24 10:19:13 -0700

    another fix

--- dbt/include/databricks/macros/relations/location.sql
+++ dbt/include/databricks/macros/relations/location.sql
@@ -17,7 +17,9 @@
   {%- set identifier = model['alias'] -%}
   {%- if location_root is not none %}
   {%- set model_path = adapter.compute_external_path(config, model, is_incremental()) %}
+    {%- if model_path != '' -%}
     location '{{ model_path }}'
+    {%- endif -%}
   {%- elif (not relation.is_hive_metastore()) and file_format != 'delta' -%}
     {{ exceptions.raise_compiler_error(
         'Incompatible configuration: `location_root` must be set when using a non-delta file format with Unity Catalog'
